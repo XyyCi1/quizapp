c340a80406c9be30337cf7a48f7f229fca569140
XyyCi1 <bavikx@gmail.com>
2025-10-16 22:27:05 +0800

    Add project files.

---
+++ quizapp/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblQuestion = New System.Windows.Forms.Label()
+        Me.rbA = New System.Windows.Forms.RadioButton()
+        Me.rbB = New System.Windows.Forms.RadioButton()
+        Me.rbC = New System.Windows.Forms.RadioButton()
+        Me.rbD = New System.Windows.Forms.RadioButton()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Checkerlbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblQuestion
+        '
+        Me.lblQuestion.AutoSize = True
+        Me.lblQuestion.Location = New System.Drawing.Point(221, 115)
+        Me.lblQuestion.Name = "lblQuestion"
+        Me.lblQuestion.Size = New System.Drawing.Size(39, 13)
+        Me.lblQuestion.TabIndex = 0
+        Me.lblQuestion.Text = "Label1"
+        '
+        'rbA
+        '
+        Me.rbA.AutoSize = True
+        Me.rbA.Location = New System.Drawing.Point(224, 145)
+        Me.rbA.Name = "rbA"
+        Me.rbA.Size = New System.Drawing.Size(32, 17)
+        Me.rbA.TabIndex = 1
+        Me.rbA.TabStop = True
+        Me.rbA.Text = "A"
+        Me.rbA.UseVisualStyleBackColor = True
+        '
+        'rbB
+        '
+        Me.rbB.AutoSize = True
+        Me.rbB.Location = New System.Drawing.Point(224, 168)
+        Me.rbB.Name = "rbB"
+        Me.rbB.Size = New System.Drawing.Size(32, 17)
+        Me.rbB.TabIndex = 1
+        Me.rbB.TabStop = True
+        Me.rbB.Text = "B"
+        Me.rbB.UseVisualStyleBackColor = True
+        '
+        'rbC
+        '
+        Me.rbC.AutoSize = True
+        Me.rbC.Location = New System.Drawing.Point(224, 191)
+        Me.rbC.Name = "rbC"
+        Me.rbC.Size = New System.Drawing.Size(32, 17)
+        Me.rbC.TabIndex = 1
+        Me.rbC.TabStop = True
+        Me.rbC.Text = "C"
+        Me.rbC.UseVisualStyleBackColor = True
+        '
+        'rbD
+        '
+        Me.rbD.AutoSize = True
+        Me.rbD.Location = New System.Drawing.Point(224, 214)
+        Me.rbD.Name = "rbD"
+        Me.rbD.Size = New System.Drawing.Size(33, 17)
+        Me.rbD.TabIndex = 1
+        Me.rbD.TabStop = True
+        Me.rbD.Text = "D"
+        Me.rbD.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(208, 237)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 2
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Location = New System.Drawing.Point(225, 333)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(35, 13)
+        Me.lblScore.TabIndex = 0
+        Me.lblScore.Text = "Score"
+        '
+        'Checkerlbl
+        '
+        Me.Checkerlbl.AutoSize = True
+        Me.Checkerlbl.Location = New System.Drawing.Point(218, 272)
+        Me.Checkerlbl.Name = "Checkerlbl"
+        Me.Checkerlbl.Size = New System.Drawing.Size(47, 13)
+        Me.Checkerlbl.TabIndex = 3
+        Me.Checkerlbl.Text = "Checker"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Checkerlbl)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.rbD)
+        Me.Controls.Add(Me.rbC)
+        Me.Controls.Add(Me.rbB)
+        Me.Controls.Add(Me.rbA)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.lblQuestion)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblQuestion As Label
+    Friend WithEvents rbA As RadioButton
+    Friend WithEvents rbB As RadioButton
+    Friend WithEvents rbC As RadioButton
+    Friend WithEvents rbD As RadioButton
+    Friend WithEvents btnNext As Button
+    Friend WithEvents lblScore As Label
+    Friend WithEvents Checkerlbl As Label
+End Class
---
+++ quizapp/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+
+    Private questions() As String = {
+        "1. What is the capital of Canada?",
+        "2. Which planet is known as the Red Planet?",
+        "3. Who wrote the play 'Romeo and Juliet'?",
+        "4. What is the largest mammal in the world?",
+        "5. Which element has the chemical symbol 'O'?",
+        "6. What is the currency of Japan?",
+        "7. Which continent is the Sahara Desert located in?",
+        "8. Who painted the Mona Lisa?",
+        "9. What is the boiling point of water at sea level in Celsius?",
+        "10. Which organ in the human body is responsible for pumping blood?",
+        "11. What is the smallest prime number?",
+        "12. What is the longest river in the world?"
+    }
+
+    Private choices(,) As String = {
+        {"A) Toronto", "B) Vancouver", "C) Ottawa", "D) Montreal"},
+        {"A) Venus", "B) Mars", "C) Jupiter", "D) Saturn"},
+        {"A) Charles Dickens", "B) William Shakespeare", "C) Jane Austen", "D) Mark Twain"},
+        {"A) African Elephant", "B) Blue Whale", "C) Giraffe", "D) Hippopotamus"},
+        {"A) Gold", "B) Oxygen", "C) Osmium", "D) Oganesson"},
+        {"A) Won", "B) Yuan", "C) Yen", "D) Ringgit"},
+        {"A) Asia", "B) Africa", "C) Australia", "D) South America"},
+        {"A) Vincent van Gogh", "B) Pablo Picasso", "C) Leonardo da Vinci", "D) Claude Monet"},
+        {"A) 90°C", "B) 100°C", "C) 120°C", "D) 80°C"},
+        {"A) Brain", "B) Liver", "C) Heart", "D) Kidney"},
+        {"A) 0", "B) 1", "C) 2", "D) 3"},
+        {"A) Amazon", "B) Nile", "C) Yangtze", "D) Mississippi"}
+    }
+
+    Private answers() As String = {
+        "C", "B", "B", "B", "B", "C", "B", "C", "B", "C", "C", "B"
+    }
+
+    Private currentQuestion As Integer = 0
+    Private score As Integer = 0
+    Private Sub LoadQuestion()
+        lblQuestion.Text = questions(currentQuestion)
+        rbA.Text = choices(currentQuestion, 0)
+        rbB.Text = choices(currentQuestion, 1)
+        rbC.Text = choices(currentQuestion, 2)
+        rbD.Text = choices(currentQuestion, 3)
+
+        rbA.Checked = False
+        rbB.Checked = False
+        rbC.Checked = False
+        rbD.Checked = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadQuestion()
+    End Sub
+
+
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        Dim selected As String = ""
+
+        If rbA.Checked Then
+            selected = "A"
+        End If
+        If rbB.Checked Then
+            selected = "B"
+        End If
+        If rbC.Checked Then
+            selected = "C"
+        End If
+        If rbD.Checked Then
+            selected = "D"
+        End If
+
+        If selected = answers(currentQuestion) Then
+            score += 1
+            Checkerlbl.Text = "Previous was Correct!"
+        Else
+            Checkerlbl.Text = "Previous was Wrong!"
+        End If
+
+        currentQuestion += 1
+
+        If currentQuestion < questions.Length Then
+            LoadQuestion()
+        Else
+            lblQuestion.Text = "Quiz complete!"
+            lblScore.Text = "Your score: " & score & " out of " & questions.Length
+            btnNext.Enabled = False
+        End If
+    End Sub
+
+
+
+
+End Class
